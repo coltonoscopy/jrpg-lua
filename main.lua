5efe9ece6a8f99f21cb90e6494322c7a4bb9cbcc
Colton Ogden <coltonoscopy@gmail.com>
2017-03-04 16:43:28 -0500

    displaying a grass tile

--- main.lua
+++ main.lua
@@ -1,27 +1,33 @@
 local w
 local h
 
+-- virtual resolution coords
+vw = 320
+vh = 180
+
+local canvas
+local font
+local tileSprite
+
 function love.load()
     love.window.setFullscreen(true)
     w = love.graphics.getWidth()
     h = love.graphics.getHeight()
 
-    -- virtual resolution coords
-    vw = 320
-    vh = 180
-
-    -- draw to render texture for virtual resolution
     canvas = love.graphics.newCanvas(vw, vh)
     canvas:setFilter("nearest", "nearest")
-    local font = love.graphics.newFont("Altima.ttf", 16)
+
+    font = love.graphics.newFont("fonts/Altima.ttf", 16)
     font:setFilter("nearest", "nearest")
     love.graphics.setFont(font)
 
-    love.graphics.setCanvas(canvas)
-        love.graphics.printf('Hello Butt', 0, vh / 2, vw, "center")
-    love.graphics.setCanvas()
+    tileSprite = love.graphics.newImage("graphics/grass_tile.png")
 end
 
 function love.draw()
-    love.graphics.draw(canvas, 0, 0, 0, 6)
+    love.graphics.setCanvas(canvas)
+        love.graphics.draw(tileSprite)
+    love.graphics.setCanvas()
+
+    love.graphics.draw(canvas, 0, 0, 0, w / vw)
 end
