feb61ab4ac184abf40ad7a47a60a01486e0aeb6a
Colton Ogden <coltonoscopy@gmail.com>
2017-03-04 15:44:34 -0500

    centered text with virtual resolution

--- main.lua
+++ main.lua
@@ -5,8 +5,23 @@ function love.load()
     love.window.setFullscreen(true)
     w = love.graphics.getWidth()
     h = love.graphics.getHeight()
+
+    -- virtual resolution coords
+    vw = 320
+    vh = 180
+
+    -- draw to render texture for virtual resolution
+    canvas = love.graphics.newCanvas(vw, vh)
+    canvas:setFilter("nearest", "nearest")
+    local font = love.graphics.newFont("Altima.ttf", 16)
+    font:setFilter("nearest", "nearest")
+    love.graphics.setFont(font)
+
+    love.graphics.setCanvas(canvas)
+        love.graphics.printf('Hello Butt', 0, vh / 2, vw, "center")
+    love.graphics.setCanvas()
 end
 
 function love.draw()
-    love.graphics.printf('Hello World', 0, h / 2, w, "center")
+    love.graphics.draw(canvas, 0, 0, 0, 6)
 end
