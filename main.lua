56721f0b9f2421b7399f696308cb94e2de6c4ae5
Colton Ogden <coltonoscopy@gmail.com>
2017-03-04 17:14:47 -0500

    tiling sprites with dynamic virtual resolution

--- main.lua
+++ main.lua
@@ -1,20 +1,29 @@
-local w
-local h
+local displayWidth
+local displayHeight
 
 -- virtual resolution coords
-vw = 320
-vh = 180
+local virtualWidth
+local virtualHeight
 
 local canvas
 local font
 local tileSprite
 
+local tilesPerRow
+local tilesPerColumn
+
+local tileWidth
+local tileHeight
+
 function love.load()
     love.window.setFullscreen(true)
-    w = love.graphics.getWidth()
-    h = love.graphics.getHeight()
+    displayWidth = love.graphics.getWidth()
+    displayHeight = love.graphics.getHeight()
 
-    canvas = love.graphics.newCanvas(vw, vh)
+    virtualWidth = displayWidth / 6
+    virtualHeight = displayHeight / 6
+
+    canvas = love.graphics.newCanvas(virtualWidth, virtualHeight)
     canvas:setFilter("nearest", "nearest")
 
     font = love.graphics.newFont("fonts/Altima.ttf", 16)
@@ -22,12 +31,21 @@ function love.load()
     love.graphics.setFont(font)
 
     tileSprite = love.graphics.newImage("graphics/grass_tile.png")
+    tileWidth = tileSprite:getWidth()
+    tileHeight = tileSprite:getHeight()
+
+    tilesPerRow = math.ceil(virtualWidth / tileWidth)
+    tilesPerColumn = math.ceil(virtualHeight / tileHeight)
 end
 
 function love.draw()
     love.graphics.setCanvas(canvas)
-        love.graphics.draw(tileSprite)
+        for j = 0, tilesPerColumn - 1 do
+            for i = 0, tilesPerRow - 1 do
+                love.graphics.draw(tileSprite, i * tileWidth, j * tileHeight)
+            end
+        end
     love.graphics.setCanvas()
 
-    love.graphics.draw(canvas, 0, 0, 0, w / vw)
+    love.graphics.draw(canvas, 0, 0, 0, displayWidth / virtualWidth)
 end
